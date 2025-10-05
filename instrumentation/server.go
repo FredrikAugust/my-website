9e273572b971fbce2ec5362a57ebb179c7840c62
Fredrik August Madsen-Malmo <fredrik.malmo@icloud.com>
2025-10-05 19:29:40 +0200

    implement support for traceparent

--- instrumentation/server.go
+++ instrumentation/server.go
@@ -8,6 +8,8 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/propagation"
 	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
 	"go.opentelemetry.io/otel/trace"
 )
@@ -18,11 +20,18 @@ func InstrumentRouter(chi chi.Router) chi.Router {
 
 	return chi.With(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ctx, span := tracer.Start(r.Context(), fmt.Sprintf("%s %s", r.Method, r.Pattern), trace.WithAttributes(
-				requestVerbAttribute(r),
-				semconv.ServerAddress(r.Host),
-				semconv.URLFull(r.URL.String()),
-			))
+			ctx := otel.GetTextMapPropagator().Extract(r.Context(), propagation.HeaderCarrier(r.Header))
+
+			ctx, span := tracer.Start(
+				ctx,
+				fmt.Sprintf("%s %s", r.Method, r.Pattern),
+				trace.WithSpanKind(trace.SpanKindServer),
+				trace.WithAttributes(
+					requestVerbAttribute(r),
+					semconv.ServerAddress(r.Host),
+					semconv.URLFull(r.URL.String()),
+				),
+			)
 			defer span.End()
 
 			// By default we don't have access to the responses from the http writer
@@ -37,6 +46,12 @@ func InstrumentRouter(chi chi.Router) chi.Router {
 				semconv.HTTPResponseStatusCode(ww.Status()),
 				semconv.HTTPResponseBodySize(ww.BytesWritten()),
 			)
+
+			if ww.Status() >= 200 && ww.Status() < 400 {
+				span.SetStatus(codes.Ok, "")
+			} else if ww.Status() >= 400 {
+				span.SetStatus(codes.Error, "the server returned a status code > 400")
+			}
 		})
 	})
 }
