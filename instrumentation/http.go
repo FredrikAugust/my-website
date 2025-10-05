0fe25f2bc7d5927c6cb6d7c17eb9d4b3f59880a1
Fredrik August Madsen-Malmo <fredrik.malmo@icloud.com>
2025-10-05 23:45:34 +0200

    Revert "don't include cf"
    This reverts commit 187a606a81fa181c5fba350bf1b97a55dffbb5e4.

--- instrumentation/http.go
+++ instrumentation/http.go
@@ -33,6 +33,7 @@ func InstrumentRouter(chi chi.Router) chi.Router {
 					semconv.ServerAddress(r.Host),
 					semconv.URLFull(r.URL.String()),
 					semconv.UserAgentOriginal(r.Header.Get("user-agent")),
+					semconv.ClientAddress(r.Header.Get("Cf-Connecting-Ip")), // Cloudflare specific
 				),
 			)
 			defer span.End()
