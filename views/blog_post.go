a5b64a769f233ee5a1467cf4c30b7cf32666ec77
Fredrik August Madsen-Malmo <fredrik.malmo@icloud.com>
2025-11-07 17:24:36 +0100

    style links

--- views/blog_post.go
+++ views/blog_post.go
@@ -24,7 +24,7 @@ func BlogPost(authenticated bool, post model.BlogPost) g.Node {
 		route.BlogPost(post.Slug),
 		authenticated,
 		h.Section(
-			h.Class("flex flex-col gap-2 overflow-x-hidden"),
+			h.Class("flex flex-col gap-2 overflow-x-hidden [&_a]:!text-black [&_a]:underline [&_a]:!font-serif"),
 			h.A(g.Text("← Back to all blog posts"), h.Href(route.Blog), h.Class("text-sm")),
 			h.Div(
 				h.Class("flex flex-col"),
